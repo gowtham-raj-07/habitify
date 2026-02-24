0d1d4d11362dc281f435692879028a2e5ab797ef
gowtham-raj-07 <gowthamraj07gr7@gmail.com>
2026-02-24 16:12:25 +0530

    fix tailwind arbitrary apply issue

--- frontend/src/Api/Axios.jsx
+++ frontend/src/Api/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://habitify-gkcp.onrender.com/api",
 });
 
 API.interceptors.request.use(
--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -23,7 +23,7 @@ function Register() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/register",
+        "https://habitify-gkcp.onrender.com/api/auth/register",
         { name, email, password }
       );
 
